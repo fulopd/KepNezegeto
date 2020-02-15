9284db00c9a85b21f7a35338bd73ad143ffa8a29
Fülöp Dániel <fulopdaniel@gmail.com>
2020-02-15 13:29:17 +0100

    Befejezve

--- KepNezegeto/Form1.Designer.cs
+++ KepNezegeto/Form1.Designer.cs
@@ -32,10 +32,13 @@
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.checkBox1 = new System.Windows.Forms.CheckBox();
             this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.buttonMegjelenites = new System.Windows.Forms.Button();
-            this.buttonTorles = new System.Windows.Forms.Button();
-            this.buttonHatter = new System.Windows.Forms.Button();
             this.buttonBezaras = new System.Windows.Forms.Button();
+            this.buttonHatter = new System.Windows.Forms.Button();
+            this.buttonTorles = new System.Windows.Forms.Button();
+            this.buttonMegjelenites = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.labelUserName = new System.Windows.Forms.Label();
             this.tableLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.flowLayoutPanel1.SuspendLayout();
@@ -55,7 +58,7 @@
             this.tableLayoutPanel1.RowCount = 2;
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(534, 291);
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(534, 336);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
             // pictureBox1
@@ -65,82 +68,102 @@
             this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.Location = new System.Drawing.Point(3, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(528, 255);
+            this.pictureBox1.Size = new System.Drawing.Size(528, 296);
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
             // checkBox1
             // 
+            this.checkBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(3, 264);
+            this.checkBox1.Location = new System.Drawing.Point(10, 310);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(60, 17);
             this.checkBox1.TabIndex = 1;
             this.checkBox1.Text = "Kitöltés";
             this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // flowLayoutPanel1
             // 
+            this.flowLayoutPanel1.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.flowLayoutPanel1.Controls.Add(this.buttonBezaras);
             this.flowLayoutPanel1.Controls.Add(this.buttonHatter);
             this.flowLayoutPanel1.Controls.Add(this.buttonTorles);
             this.flowLayoutPanel1.Controls.Add(this.buttonMegjelenites);
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Controls.Add(this.labelUserName);
             this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(83, 264);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(83, 307);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(448, 24);
             this.flowLayoutPanel1.TabIndex = 2;
             // 
-            // buttonMegjelenites
-            // 
-            this.buttonMegjelenites.AutoSize = true;
-            this.buttonMegjelenites.Location = new System.Drawing.Point(124, 3);
-            this.buttonMegjelenites.Name = "buttonMegjelenites";
-            this.buttonMegjelenites.Size = new System.Drawing.Size(78, 23);
-            this.buttonMegjelenites.TabIndex = 0;
-            this.buttonMegjelenites.Text = "Megjelenítés";
-            this.buttonMegjelenites.UseVisualStyleBackColor = true;
-            this.buttonMegjelenites.Click += new System.EventHandler(this.buttonMegjelenites_Click);
-            // 
-            // buttonTorles
-            // 
-            this.buttonTorles.AutoSize = true;
-            this.buttonTorles.Location = new System.Drawing.Point(208, 3);
-            this.buttonTorles.Name = "buttonTorles";
-            this.buttonTorles.Size = new System.Drawing.Size(75, 23);
-            this.buttonTorles.TabIndex = 0;
-            this.buttonTorles.Text = "Törlés";
-            this.buttonTorles.UseVisualStyleBackColor = true;
-            this.buttonTorles.Click += new System.EventHandler(this.buttonTorles_Click);
-            // 
-            // buttonHatter
-            // 
-            this.buttonHatter.AutoSize = true;
-            this.buttonHatter.Location = new System.Drawing.Point(289, 3);
-            this.buttonHatter.Name = "buttonHatter";
-            this.buttonHatter.Size = new System.Drawing.Size(75, 23);
-            this.buttonHatter.TabIndex = 0;
-            this.buttonHatter.Text = "Háttér";
-            this.buttonHatter.UseVisualStyleBackColor = true;
-            this.buttonHatter.Click += new System.EventHandler(this.buttonHatter_Click);
-            // 
             // buttonBezaras
             // 
-            this.buttonBezaras.AutoSize = true;
+            this.buttonBezaras.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.buttonBezaras.Location = new System.Drawing.Point(370, 3);
             this.buttonBezaras.Name = "buttonBezaras";
-            this.buttonBezaras.Size = new System.Drawing.Size(75, 23);
+            this.buttonBezaras.Size = new System.Drawing.Size(75, 20);
             this.buttonBezaras.TabIndex = 0;
             this.buttonBezaras.Text = "Bezárás";
             this.buttonBezaras.UseVisualStyleBackColor = true;
             this.buttonBezaras.Click += new System.EventHandler(this.buttonBezaras_Click);
             // 
+            // buttonHatter
+            // 
+            this.buttonHatter.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonHatter.Location = new System.Drawing.Point(289, 3);
+            this.buttonHatter.Name = "buttonHatter";
+            this.buttonHatter.Size = new System.Drawing.Size(75, 20);
+            this.buttonHatter.TabIndex = 0;
+            this.buttonHatter.Text = "Háttér";
+            this.buttonHatter.UseVisualStyleBackColor = true;
+            this.buttonHatter.Click += new System.EventHandler(this.buttonHatter_Click);
+            // 
+            // buttonTorles
+            // 
+            this.buttonTorles.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonTorles.Location = new System.Drawing.Point(208, 3);
+            this.buttonTorles.Name = "buttonTorles";
+            this.buttonTorles.Size = new System.Drawing.Size(75, 20);
+            this.buttonTorles.TabIndex = 0;
+            this.buttonTorles.Text = "Törlés";
+            this.buttonTorles.UseVisualStyleBackColor = true;
+            this.buttonTorles.Click += new System.EventHandler(this.buttonTorles_Click);
+            // 
+            // buttonMegjelenites
+            // 
+            this.buttonMegjelenites.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.buttonMegjelenites.Location = new System.Drawing.Point(124, 3);
+            this.buttonMegjelenites.Name = "buttonMegjelenites";
+            this.buttonMegjelenites.Size = new System.Drawing.Size(78, 20);
+            this.buttonMegjelenites.TabIndex = 0;
+            this.buttonMegjelenites.Text = "Megjelenítés";
+            this.buttonMegjelenites.UseVisualStyleBackColor = true;
+            this.buttonMegjelenites.Click += new System.EventHandler(this.buttonMegjelenites_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All file" +
+    "s (*.*)|*.*";
+            this.openFileDialog1.Title = "Megnyitás";
+            // 
+            // labelUserName
+            // 
+            this.labelUserName.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.labelUserName.AutoSize = true;
+            this.labelUserName.Location = new System.Drawing.Point(83, 6);
+            this.labelUserName.Name = "labelUserName";
+            this.labelUserName.Size = new System.Drawing.Size(35, 13);
+            this.labelUserName.TabIndex = 1;
+            this.labelUserName.Text = "label1";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(534, 291);
+            this.ClientSize = new System.Drawing.Size(534, 336);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Képnézegető";
@@ -163,6 +186,9 @@
         private System.Windows.Forms.Button buttonTorles;
         private System.Windows.Forms.Button buttonHatter;
         private System.Windows.Forms.Button buttonBezaras;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Label labelUserName;
     }
 }
 
--- KepNezegeto/Form1.cs
+++ KepNezegeto/Form1.cs
@@ -15,26 +15,50 @@ namespace KepNezegeto
         public Form1()
         {
             InitializeComponent();
+            labelUserName.Text = Environment.UserName;
         }
 
         private void buttonMegjelenites_Click(object sender, EventArgs e)
         {
-
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Load(openFileDialog1.FileName);
+            }
         }
 
         private void buttonTorles_Click(object sender, EventArgs e)
         {
-
+            pictureBox1.Image = null;
         }
 
         private void buttonHatter_Click(object sender, EventArgs e)
         {
-
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.BackColor = colorDialog1.Color;                
+            }
         }
 
         private void buttonBezaras_Click(object sender, EventArgs e)
         {
+            //Jelenlegi formot bezárja
+            //this.Close();
+            //Teljes alkalmazást bezárja
+            Application.Exit();
+            //Kliens gépen zárja be az alkalmazást
+            //Environment.Exit(0);
+        }
 
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            else
+            {
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
+            }
         }
     }
 }
